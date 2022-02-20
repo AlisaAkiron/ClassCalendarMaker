2b7e7a71cb22a7021f9aebdfa9a4dae10c1b1492
Liam Sho <conan_creeper@outlook.com>
2022-02-20 16:14:29 +0800

    chore.Add JsonConvertor for TimeOnly and DayOfWeek type

---
+++ ClassCalendarMaker.Core/Convertor/DayOfWeekJsonConvertor.cs
@@ -0,0 +1,51 @@
+﻿// ClassCalendarMaker - Convert class scheduler to calendar ICS file
+// Copyright (C) 2021 Liam Sho and Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, 
+// or any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ClassCalendarMaker.Core.Convertor;
+
+internal class DayOfWeekJsonConvertor : JsonConverter<DayOfWeek>
+{
+    public override DayOfWeek Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var dowNumber = reader.GetInt32();
+        return dowNumber switch
+        {
+            1 => DayOfWeek.Monday,
+            2 => DayOfWeek.Tuesday,
+            3 => DayOfWeek.Wednesday,
+            4 => DayOfWeek.Thursday,
+            5 => DayOfWeek.Friday,
+            6 => DayOfWeek.Saturday,
+            7 => DayOfWeek.Sunday,
+            _ => throw new ArgumentOutOfRangeException(nameof(reader), $"Day of week number can't be {dowNumber}")
+        };
+    }
+
+    public override void Write(Utf8JsonWriter writer, DayOfWeek value, JsonSerializerOptions options)
+    {
+        var writeValue = value switch
+        {
+            DayOfWeek.Sunday => 7,
+            DayOfWeek.Monday => 1,
+            DayOfWeek.Tuesday => 2,
+            DayOfWeek.Wednesday => 3,
+            DayOfWeek.Thursday => 4,
+            DayOfWeek.Friday => 5,
+            DayOfWeek.Saturday => 6,
+            _ => throw new ArgumentOutOfRangeException(nameof(value), $"Invalid DayOfWeek enum value")
+        };
+
+        writer.WriteNumberValue(writeValue);
+    }
+}
---
+++ ClassCalendarMaker.Core/Convertor/TimeOnlyJsonConvertor.cs
@@ -0,0 +1,49 @@
+﻿// ClassCalendarMaker - Convert class scheduler to calendar ICS file
+// Copyright (C) 2021 Liam Sho and Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, 
+// or any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ClassCalendarMaker.Core.Convertor;
+
+internal class TimeOnlyJsonConvertor : JsonConverter<TimeOnly>
+{
+    public override TimeOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var timeString = reader.GetString();
+
+        try
+        {
+            var splitTimeString = timeString.Split(":");
+
+            if (splitTimeString.Length != 3)
+            {
+                throw new Exception();
+            }
+
+            var hh = Convert.ToInt32(splitTimeString[0]);
+            var mm = Convert.ToInt32(splitTimeString[1]);
+            var ss = Convert.ToInt32(splitTimeString[2]);
+
+            return new TimeOnly(hh, mm, ss);
+        }
+        catch (Exception)
+        {
+            throw new JsonException($"Can't serilize {timeString} to System.TimeOnly");
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, TimeOnly value, JsonSerializerOptions options)
+    {
+        var str = value.ToLongTimeString();
+        writer.WriteStringValue(str);
+    }
+}
